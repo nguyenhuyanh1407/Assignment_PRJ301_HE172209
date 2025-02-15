5bc223f1696a9e88942639fe951a9a15f2c13c0a
nguyenhuyanh1407 <nguyenhuyanh1407@gmail.com>
2025-02-15 11:24:01 +0700

    Google Login Controller
    - Google Login function
    - Demo display name, email, avatar user login
     !Delete Client ID and Secret Key to push

---
+++ LRManagementSystem/src/java/constant/Iconstant.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package constant;
+
+/**
+ *
+ * @author ibm
+ */
+public class Iconstant {
+    
+    
+    public static final String GOOGLE_REDIRECT_URI = "http://localhost:9999/LRManagementSystem/login";
+    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
+    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    
+}
